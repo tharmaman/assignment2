3c59bd41a8714e378e726ee44346b9602966d782
tharmaman <daniel.tharmathurai@mail.mcgill.ca>
2017-11-13 15:43:41 -0500

    Update rossanda_tharmathurai_script.R

--- rossanda_tharmathurai_script.R
+++ rossanda_tharmathurai_script.R
@@ -8,14 +8,17 @@ library("ggplot2")
 # dependent variables: 
 # pct_raised (continuous variable) 
 # and success (dichotomous variable).
-
+  
 # predictor variables:
 # category, city, state, goal, fb_total_likes,
 # duration, start_month, success_month, prior campaigns, 
 # number of perks
 
 #Using a histogram for the continuous variable (pct_raised)
-hist(crowd_data$pct_raised)
+ggplot(crowd_data,aes(pct_raised)) + geom_histogram(binwidth = .05, fill="#FF9999", colour="black") + xlim(0, 2) + xlab("Percentage Raised") + ylab("Frequency") + ggtitle("Percentage Raised Histogram") + theme(plot.title = element_text(hjust = 0.5))
 
-#Using a bar plot for the continuous variable (success)
-ggplot(data = crowd_data) + geom_bar(aes(success)) + labs(x = "success", y = "Frequency")
+#Using a bar plot for the discrete variable (success)
+ggplot(crowd_data, aes(x = success)) + geom_bar(aes(y = (..count..)/sum(..count..))) + scale_y_continuous(formatter = 'percent')
+
+
+
