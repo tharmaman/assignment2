60ef1a70796cccb6c38a518d2d348fac422edaad
sharon3895 <33530553+sharon3895@users.noreply.github.com>
2017-11-15 19:03:37 -0500

    Update question 3.R

--- question 3.R
+++ question 3.R
@@ -1,19 +1,21 @@
 crowd_data <- read_xlsx("~/Downloads/Crowdfunding_data.xlsx")
-summary(crowd_data)
-crowd_data <- crowd_data[,c("success","city","state","goal","fb_total_likes","category","unique_visitors","start_month","end_month","prior_campaigns","num_perks")]
-str(crowd_data)
+crowd_data <- crowd_data[,c("success","state","goal","fb_total_likes","category","unique_visitors","start_month","end_month","prior_campaigns","num_perks")]
+
+crowd_data$state <-as.factor(crowd_data$state)
+crowd_data$category <-as.factor(crowd_data$category)
+crowd_data$start_month <-as.factor(crowd_data$start_month)
+crowd_data$end_month <-as.factor(crowd_data$end_month)
+crowd_data$prior_campaigns <-as.factor(crowd_data$prior_campaigns)
+crowd_data$num_perks <-as.factor(crowd_data$num_perks)
 
 library(caTools)
-set.seed(2)
+set.seed(100)
 
 sample = sample.split(crowd_data, SplitRatio = 0.6)
 CDtrain<- subset(crowd_data,sample == TRUE)
-CDtest <- subset(crowd_data, sample ==FALSE)
+CDtest <- subset(crowd_data, sample == FALSE)
 
-install.packages("rpart")
-library(rpart)
-
-install.packages("rpart.plot")
+library("rpart")
 library("rpart.plot")
 library("caret")
 library("e1071")
@@ -23,12 +25,12 @@ tree_1<-rpart(success ~ .,
 rpart.plot(tree_1,type=1, extra = 2,under = TRUE)
 title(sub = "Classification Tree with minsplit = 200")
 
-tree_2<-rpart(success ~ .,
+tree_2<-rpart(success ~ ., 
               data = CDtrain, method = "class", control = rpart.control(minsplit = 100))
 rpart.plot(tree_2,type=1, extra = 2,under = TRUE)
 title(sub = "Classification Tree with minsplit = 100")
 
-tree_3<-rpart(success ~ .,
+tree_3<-rpart(success ~ ., 
               data = CDtrain, method = "class", control = rpart.control(minsplit = 50))
 rpart.plot(tree_3,type=1, extra = 2,under = TRUE)
 title(sub = "Classification Tree with minsplit = 50")
@@ -41,6 +43,3 @@ confusionMatrix(pred2, CDtest$success, dnn = c("Predicted","Actual"))
 
 pred3<- predict(tree_3, CDtest, type="class")
 confusionMatrix(pred3, CDtest$success, dnn = c("Predicted","Actual"))
-
-
-
